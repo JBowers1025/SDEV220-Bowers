bd3e72d3635e02ed7ea39c877163500c328e4f7c
Jenn Bowers <jbowers80@ivytech.edu>
2025-11-04 14:12:27 -0500

    Tweaked code
    Changed code to include a defined function. More comments added.

--- M02CaseStudyBowers.py
+++ M02CaseStudyBowers.py
@@ -3,10 +3,15 @@
 # M02CaseStudyBowers.py
 # This program determines if a student is part of the dean's list and honor roll.
 
-print("\nThis will determine if the student has made it onto the Dean's list and or Honor Roll.\n")
+import os
+os.system("cls") # Clears the ouput (terminal) screen
+
+print("\nThis will determine if the student has made it onto the Dean's list and/or Honor Roll.\n")
+# Beginning input to start the program
 lastName = input("Please enter the student's last name: ")
 
-while lastName != "ZZZ": # If the value is ZZZ the program will quit.
+# Repeating block of code until the lastName is inputted as 
+while lastName != "ZZZ" and lastName != "zzz": # If the value is ZZZ or zzz the program will quit.
     firstName = input("Please enter the student's first name: ")
     gpa = input("Please enter the student's GPA: ")
 
@@ -19,13 +24,13 @@ while lastName != "ZZZ": # If the value is ZZZ the program will quit.
             if 3.5 <= gpa: # Tests if on the Dean's list
                 print(firstName + " " + lastName + " has made it on the Dean's list and the Honor Roll!\n")
 
-            if 3.25 <= gpa < 3.5: # Tests if on the Honor Roll
+            elif 3.25 <= gpa < 3.5: # Tests if on the Honor Roll
                 print(firstName + " " + lastName + " has made it on the Honor Roll!\n")
 
-            elif gpa < 3.25: # Returns a statement that the student wasn't on either list
+            else: # (gpa < 3.25) Returns a statement that the student wasn't on either list
                 print("Sorry, " + firstName + " " + lastName + " has not made it on either list.\n")
 
-        else: # Returns that the GPA was not a valid number
+        else: # Returns that the GPA was not in the correct number range.
             print("The gpa value of " + str(gpa) + " is not value between 0.0 and 4.0.\n")
 
     except ValueError: # Returns that the GPA was not a float
@@ -33,5 +38,4 @@ while lastName != "ZZZ": # If the value is ZZZ the program will quit.
 
     lastName = input("Please enter the next student's last name: ") # Repeats the program
 
-print("Thanks for using this program. Have a great day!") # Ends the program
-
+print("\nThanks for using this program. Have a great day!\n") # Ends the program
